d8efae6ed03184aada7d471bc1901dab78396735
«Slavyanus <slavyanus@inbox.ru>
2022-09-15 02:12:54 +0700

    formiruem massiv

--- Program.cs
+++ Program.cs
@@ -12,4 +12,16 @@ string[] array =
     "eeeee",
     "z"
 };
+
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+
  
